19305e72a7f7e82dc4c374d4e9ef7cdd4984edac
andrei <protoandrei@protonmail.com>
2026-04-07 20:07:47 +0200

    Refactor `fix_manifest_website` to simplify arguments and add file filtering in pre-commit config

--- tools/fix_manifest_website.py
+++ tools/fix_manifest_website.py
@@ -10,37 +10,33 @@ WEBSITE_KEY_RE = re.compile(r"""(["']website["']\s*:\s*["'])([^"']*)(["'])""")
 
 @click.command()
 @click.argument("url")
-@click.argument("addons", nargs=-1, type=click.Path())
-@click.option("--addons-dir", default=".")
-def main(url, addons, addons_dir):
-    for addon_dir in addons:
-        manifest_path = get_manifest_path(os.path.join(addons_dir, addon_dir))
-        if not manifest_path:
-            continue
+@click.argument("manifest_files", nargs=-1, type=click.Path())
+def main(url, manifest_files):
+    for file in manifest_files:
         try:
-            with open(manifest_path) as manifest_file:
+            with open(file) as manifest_file:
                 manifest = parse_manifest(manifest_file.read())
         except Exception:
             raise click.ClickException(
-                "Error parsing manifest {}.".format(manifest_path)
+                "Error parsing manifest {}.".format(manifest_file)
             )
         if "website" not in manifest:
             raise click.ClickException(
-                "website key not found in manifest in {}.".format(addon_dir)
+                "website key not found in manifest in {}.".format(manifest_file)
             )
-        with open(manifest_path) as manifest_file:
+        with open(file) as manifest_file:
             manifest_str = manifest_file.read()
         new_manifest_str, n = WEBSITE_KEY_RE.subn(
             r"\g<1>" + url + r"\g<3>", manifest_str
         )
         if n == 0:
             raise click.ClickException(
-                "no website key match in manifest in {}.".format(addon_dir)
+                "no website key match in manifest in {}.".format(manifest)
             )
         if n > 1:
             raise click.ClickException(
-                "more than one website key match in manifest in {}.".format(addon_dir)
+                "more than one website key match in manifest in {}.".format(manifest)
             )
         if new_manifest_str != manifest_str:
-            with open(manifest_path, "w") as manifest_file:
+            with open(file, "w") as manifest_file:
                 manifest_file.write(new_manifest_str)
