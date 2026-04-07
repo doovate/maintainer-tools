e8c8128cfa66c9c579d60624592b6bc6b69a82ac
andrei <protoandrei@protonmail.com>
2026-04-07 19:44:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Doovate maintainer tools
+
+This repository contains tools for maintaining Doovate's Odoo addons.
---
+++ tools/fix_manifest_website.py
@@ -0,0 +1,46 @@
+import os
+import re
+
+import click
+
+from .manifest import get_manifest_path, parse_manifest
+
+WEBSITE_KEY_RE = re.compile(r"""(["']website["']\s*:\s*["'])([^"']*)(["'])""")
+
+
+@click.command()
+@click.argument("url")
+@click.argument("addons", nargs=-1, type=click.Path())
+@click.option("--addons-dir", default=".")
+def main(url, addons, addons_dir):
+    for addon_dir in addons:
+        manifest_path = get_manifest_path(os.path.join(addons_dir, addon_dir))
+        if not manifest_path:
+            continue
+        try:
+            with open(manifest_path) as manifest_file:
+                manifest = parse_manifest(manifest_file.read())
+        except Exception:
+            raise click.ClickException(
+                "Error parsing manifest {}.".format(manifest_path)
+            )
+        if "website" not in manifest:
+            raise click.ClickException(
+                "website key not found in manifest in {}.".format(addon_dir)
+            )
+        with open(manifest_path) as manifest_file:
+            manifest_str = manifest_file.read()
+        new_manifest_str, n = WEBSITE_KEY_RE.subn(
+            r"\g<1>" + url + r"\g<3>", manifest_str
+        )
+        if n == 0:
+            raise click.ClickException(
+                "no website key match in manifest in {}.".format(addon_dir)
+            )
+        if n > 1:
+            raise click.ClickException(
+                "more than one website key match in manifest in {}.".format(addon_dir)
+            )
+        if new_manifest_str != manifest_str:
+            with open(manifest_path, "w") as manifest_file:
+                manifest_file.write(new_manifest_str)
