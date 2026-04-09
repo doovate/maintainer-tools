4827d561bab6b2dd52b8aeadc663b5bf4ad86b36
andrei <protoandrei@protonmail.com>
2026-04-09 16:25:20 +0200

    Update `website.py` to track changes and exit on modifications, and enhance pre-commit hook description

--- tools/website.py
+++ tools/website.py
@@ -24,6 +24,7 @@ def main():
     filtered_addons = [
         folder for folder in os.listdir(".") if ADDON_FILTER.match(folder)
     ]
+    changed = False
     for addon in filtered_addons:
         # Create missing folder
         index_file = os.path.join(addon, "static", "description", "index.html")
@@ -34,9 +35,11 @@ def main():
         if not os.path.exists(os.path.join(addon, "static", "description")):
             print(f"Creating description folder for {addon}")
             Path(addon, "static", "description").mkdir(parents=True, exist_ok=True)
+            changed = True
         if not os.path.exists(index_file):
             print(f"Creating index.html for {addon}")
             copyfile(TEMPLATE_INDEX, index_file)
+            changed = True
         # Read index.html and check if it's the template, if the template has been updated, replace it, else keep the existing content
         if os.path.exists(index_file):
             with open(index_file, "r") as f:
@@ -44,15 +47,22 @@ def main():
             if INDEX_FIRST_LINE in lines[0] and hash("".join(lines)) != template_hash:
                 print(f"Updating index.html for {addon}")
                 copyfile(TEMPLATE_INDEX, index_file)
+                changed = True
         if not os.path.exists(asset_folder):
             print(f"Creating assets folder for {addon}")
             Path(asset_folder).mkdir(parents=True, exist_ok=True)
+            changed = True
         if not os.path.exists(icon_file):
             print(f"Creating icon.png for {addon}")
             copyfile(TEMPLATE_ICON, icon_file)
+            changed = True
         if not os.path.exists(hero_file):
             print(f"Creating hero image for {addon}")
             copyfile(TEMPLATE_HERO, hero_file)
+            changed = True
         if not os.path.exists(full_logo_file):
             print(f"Creating full logo image for {addon}")
             copyfile(TEMPLATE_FULL_LOGO, full_logo_file)
+            changed = True
+    if changed:
+        raise SystemExit(1)
